edb2c4c3e3484452875d6aa616faf8c9082c1088
Knut Johannes Dahle <knut.johannes.dahle@gmail.com>
2017-11-06 14:54:51 +0100

    Changed to use github as gitlab enforces login

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+sudo apt-get update
 sudo apt-get install git python-pip -y
 sudo pip install ansible
 
 mkdir $HOME/programming
 cd $HOME/programming
-git clone https://gitlab.acando.no/mattilsynet/ubuntu-dev-bootstrap.git
+git clone https://github.com/knutjda/dev-ubuntu-bootstrap.git
+cd $HOME/programming/ubuntu-dev-bootstrap
 
+ansible-playbook setup.yml
