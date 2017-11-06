fb76a8b52a8a068261c2511e8239c08d4fabb474
Knut Johannes Dahle <knut.johannes.dahle@gmail.com>
2017-11-06 16:20:36 +0100

    Have to disable ssl verify due to proxy problems

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo pip install ansible
 
 mkdir -p $HOME/programming
 cd $HOME/programming
-git clone https://github.com/knutjda/dev-ubuntu-bootstrap.git
+git -c http.sslVerify=false clone https://github.com/knutjda/dev-ubuntu-bootstrap.git
 cd $HOME/programming/dev-ubuntu-bootstrap
 
 ansible-playbook setup.yml
